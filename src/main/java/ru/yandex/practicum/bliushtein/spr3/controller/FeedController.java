cab0646f45203c72baff32b97bd75e3656477e36
bliushtein <domovoy9812@yandex.ru>
2025-04-04 18:43:55 +0500

    - multipaging support implemented

--- src/main/java/ru/yandex/practicum/bliushtein/spr3/controller/FeedController.java
+++ src/main/java/ru/yandex/practicum/bliushtein/spr3/controller/FeedController.java
@@ -1,13 +1,15 @@
 package ru.yandex.practicum.bliushtein.spr3.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.bliushtein.spr3.core.service.PostService;
 import ru.yandex.practicum.bliushtein.spr3.core.service.dto.PostSummary;
 
 import java.util.List;
+import java.util.Objects;
 
 
 @Controller
@@ -21,10 +23,20 @@ public class FeedController {
     }
 
     @GetMapping
-    public String feed(Model model, @RequestParam(name = "tag", required = false) String tag) {
-        List<PostSummary> posts = StringUtils.isBlank(tag) ? service.findAll() : service.findByTag(tag);
-        model.addAttribute("tag", tag);
-        model.addAttribute("posts", posts);
+    public String feed(HttpSession session,
+                       @RequestParam(name = "tag", required = false) String tag,
+                       @RequestParam(name = "pageSize", required = false, defaultValue = "50") int pageSize,
+                       @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber) {
+        String searchTag = (String) session.getAttribute("tag");
+        PagedListHolder<PostSummary> pagedPosts = (PagedListHolder<PostSummary>) session.getAttribute("posts");;
+        if (!Objects.equals(searchTag, tag) || pagedPosts == null) {
+            List<PostSummary> posts = StringUtils.isBlank(tag) ? service.findAll() : service.findByTag(tag);
+            pagedPosts = new PagedListHolder<>(posts);
+            session.setAttribute("posts", pagedPosts);
+            session.setAttribute("tag", tag);
+        }
+        pagedPosts.setPageSize(pageSize);
+        pagedPosts.setPage(pageNumber);
         return "feed";
     }
 }
