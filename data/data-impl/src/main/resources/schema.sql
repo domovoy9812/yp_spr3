b1f10fc1ce557b545f4531a33032b87188c9e5d8
bliushtein <domovoy9812@yandex.ru>
2025-04-05 20:43:43 +0500

    - init db schema script updated

--- data/data-impl/src/main/resources/schema.sql
+++ data/data-impl/src/main/resources/schema.sql
@@ -1,29 +1,33 @@
-CREATE TABLE IF NOT EXISTS public.users
+-- Table: public.binary_storage
+
+-- DROP TABLE IF EXISTS public.binary_storage;
+
+CREATE TABLE IF NOT EXISTS public.binary_storage
 (
     id uuid NOT NULL DEFAULT gen_random_uuid(),
-    name character varying(30) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT users_pkey PRIMARY KEY (id),
-    CONSTRAINT users_name_key UNIQUE (name)
+    data bytea NOT NULL,
+    CONSTRAINT binary_storage_pkey PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.users
+ALTER TABLE IF EXISTS public.binary_storage
     OWNER to bliushtein_yp_sprint3;
+
+-- Table: public.posts
+
+-- DROP TABLE IF EXISTS public.posts;
 
 CREATE TABLE IF NOT EXISTS public.posts
 (
     id uuid NOT NULL DEFAULT gen_random_uuid(),
     name character varying(200) COLLATE pg_catalog."default" NOT NULL,
     full_text text COLLATE pg_catalog."default" NOT NULL,
-    created_when timestamp without time zone NOT NULL,
-    owner uuid NOT NULL,
-    CONSTRAINT posts_pkey PRIMARY KEY (id),
-    CONSTRAINT owner_fk FOREIGN KEY (owner)
-        REFERENCES public.users (id) MATCH FULL
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        DEFERRABLE
+    created_when timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    likes integer NOT NULL DEFAULT 0,
+    short_text text COLLATE pg_catalog."default" NOT NULL,
+    image_key uuid,
+    CONSTRAINT posts_pkey PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
@@ -31,38 +35,50 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.posts
     OWNER to bliushtein_yp_sprint3;
 
+-- Table: public.comments
+
+-- DROP TABLE IF EXISTS public.comments;
+
 CREATE TABLE IF NOT EXISTS public.comments
 (
     id uuid NOT NULL DEFAULT gen_random_uuid(),
     text text COLLATE pg_catalog."default" NOT NULL,
-    created_when time without time zone NOT NULL,
+    created_when time with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     post uuid NOT NULL,
-    user_id uuid NOT NULL,
     CONSTRAINT comments_pkey PRIMARY KEY (id),
     CONSTRAINT post_fk FOREIGN KEY (post)
         REFERENCES public.posts (id) MATCH SIMPLE
         ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT user_fk FOREIGN KEY (user_id)
-        REFERENCES public.users (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID
 )
 
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.comments
     OWNER to bliushtein_yp_sprint3;
+-- Index: fki_post_fk
+
+-- DROP INDEX IF EXISTS public.fki_post_fk;
+
+CREATE INDEX IF NOT EXISTS fki_post_fk
+    ON public.comments USING btree
+    (post ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Table: public.tags
+
+-- DROP TABLE IF EXISTS public.tags;
 
 CREATE TABLE IF NOT EXISTS public.tags
 (
     name character varying(30) COLLATE pg_catalog."default" NOT NULL,
     post uuid NOT NULL,
     CONSTRAINT tags_pkey PRIMARY KEY (post, name),
-    CONSTRAINT post_pk FOREIGN KEY (post)
+    CONSTRAINT post_fk FOREIGN KEY (post)
         REFERENCES public.posts (id) MATCH SIMPLE
         ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID
 )
 
 TABLESPACE pg_default;
